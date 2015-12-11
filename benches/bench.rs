28e53410941dee03f05b6830cf95bf1fd98abce3
Jonathan S <gereeter+code@gmail.com>
2015-12-11 17:54:28 -0600

    Add the find benchmarks from libcollectionstest. Performance is basically identical between the two implementations.
    test find_rand_100000_parent   ... bench:         320 ns/iter (+/- 58)
    test find_rand_100000_std      ... bench:         327 ns/iter (+/- 58)
    test find_rand_10000_parent    ... bench:         159 ns/iter (+/- 21)
    test find_rand_10000_std       ... bench:         151 ns/iter (+/- 3)
    test find_rand_100_parent      ... bench:          66 ns/iter (+/- 6)
    test find_rand_100_std         ... bench:          67 ns/iter (+/- 5)
    test find_seq_100000_parent    ... bench:         124 ns/iter (+/- 13)
    test find_seq_100000_std       ... bench:         123 ns/iter (+/- 6)
    test find_seq_10000_parent     ... bench:          99 ns/iter (+/- 3)
    test find_seq_10000_std        ... bench:          98 ns/iter (+/- 1)
    test find_seq_100_parent       ... bench:          67 ns/iter (+/- 1)
    test find_seq_100_std          ... bench:          69 ns/iter (+/- 1)
    test insert_rand_100000_parent ... bench:         376 ns/iter (+/- 57)
    test insert_rand_100000_std    ... bench:         503 ns/iter (+/- 89)
    test insert_rand_10000_parent  ... bench:         257 ns/iter (+/- 8)
    test insert_rand_10000_std     ... bench:         317 ns/iter (+/- 11)
    test insert_rand_100_parent    ... bench:         151 ns/iter (+/- 3)
    test insert_rand_100_std       ... bench:         196 ns/iter (+/- 12)
    test insert_seq_100000_parent  ... bench:         261 ns/iter (+/- 14)
    test insert_seq_100000_std     ... bench:         283 ns/iter (+/- 11)
    test insert_seq_10000_parent   ... bench:         231 ns/iter (+/- 16)
    test insert_seq_10000_std      ... bench:         243 ns/iter (+/- 8)
    test insert_seq_100_parent     ... bench:          81 ns/iter (+/- 1)
    test insert_seq_100_std        ... bench:         138 ns/iter (+/- 1)
    test iter_100000_parent        ... bench:     848,690 ns/iter (+/- 90,038)
    test iter_100000_std           ... bench:   2,605,247 ns/iter (+/- 78,827)
    test iter_1000_parent          ... bench:       5,119 ns/iter (+/- 297)
    test iter_1000_std             ... bench:      25,871 ns/iter (+/- 404)
    test iter_20_parent            ... bench:          80 ns/iter (+/- 31)
    test iter_20_std               ... bench:         628 ns/iter (+/- 65)

--- benches/bench.rs
+++ benches/bench.rs
@@ -57,6 +57,62 @@ macro_rules! map_insert_seq_bench {
     )
 }
 
+macro_rules! map_find_rand_bench {
+    ($name: ident, $n: expr, $map: ident) => (
+        #[bench]
+        pub fn $name(b: &mut ::test::Bencher) {
+            use rand::{thread_rng, Rng};
+            use test::black_box;
+
+            let mut map = $map::new();
+            let n: usize = $n;
+
+            // setup
+            let mut rng = thread_rng();
+            let mut keys: Vec<_> = (0..n).map(|_| rng.gen::<usize>() % n).collect();
+
+            for &k in &keys {
+                map.insert(k, k);
+            }
+
+            rng.shuffle(&mut keys);
+
+            // measure
+            let mut i = 0;
+            b.iter(|| {
+                let t = map.get(&keys[i]);
+                i = (i + 1) % n;
+                black_box(t);
+            })
+        }
+    )
+}
+
+macro_rules! map_find_seq_bench {
+    ($name: ident, $n: expr, $map: ident) => (
+        #[bench]
+        pub fn $name(b: &mut ::test::Bencher) {
+            use test::black_box;
+
+            let mut map = $map::new();
+            let n: usize = $n;
+
+            // setup
+            for i in 0..n {
+                map.insert(i, i);
+            }
+
+            // measure
+            let mut i = 0;
+            b.iter(|| {
+                let x = map.get(&i);
+                i = (i + 1) % n;
+                black_box(x);
+            })
+        }
+    )
+}
+
 macro_rules! map_iter_bench {
     ($name: ident, $n: expr, $map: ident) => (
         #[bench]
@@ -83,23 +139,33 @@ macro_rules! map_iter_bench {
 use btree_rewrite::map::BTreeMap as ParentMap;
 use std::collections::BTreeMap as StdMap;
 
-map_insert_rand_bench!{rand_1000000_parent, 1_000_000, ParentMap}
-map_insert_rand_bench!{rand_1000000_std   , 1_000_000, StdMap}
-map_insert_rand_bench!{rand_100000_parent, 100_000, ParentMap}
-map_insert_rand_bench!{rand_100000_std   , 100_000, StdMap}
-map_insert_rand_bench!{rand_10000_parent ,  10_000, ParentMap}
-map_insert_rand_bench!{rand_10000_std    ,  10_000, StdMap}
-map_insert_rand_bench!{rand_100_parent   ,     100, ParentMap}
-map_insert_rand_bench!{rand_100_std      ,     100, StdMap}
+map_insert_rand_bench!{insert_rand_100000_parent, 100_000, ParentMap}
+map_insert_rand_bench!{insert_rand_100000_std   , 100_000, StdMap}
+map_insert_rand_bench!{insert_rand_10000_parent ,  10_000, ParentMap}
+map_insert_rand_bench!{insert_rand_10000_std    ,  10_000, StdMap}
+map_insert_rand_bench!{insert_rand_100_parent   ,     100, ParentMap}
+map_insert_rand_bench!{insert_rand_100_std      ,     100, StdMap}
 
-map_insert_seq_bench!{seq_1000000_parent  , 1_000_000, ParentMap}
-map_insert_seq_bench!{seq_1000000_std     , 1_000_000, StdMap}
-map_insert_seq_bench!{seq_100000_parent  , 100_000, ParentMap}
-map_insert_seq_bench!{seq_100000_std     , 100_000, StdMap}
-map_insert_seq_bench!{seq_10000_parent   ,  10_000, ParentMap}
-map_insert_seq_bench!{seq_10000_std      ,  10_000, StdMap}
-map_insert_seq_bench!{seq_100_parent     ,     100, ParentMap}
-map_insert_seq_bench!{seq_100_std        ,     100, StdMap}
+map_insert_seq_bench!{insert_seq_100000_parent  , 100_000, ParentMap}
+map_insert_seq_bench!{insert_seq_100000_std     , 100_000, StdMap}
+map_insert_seq_bench!{insert_seq_10000_parent   ,  10_000, ParentMap}
+map_insert_seq_bench!{insert_seq_10000_std      ,  10_000, StdMap}
+map_insert_seq_bench!{insert_seq_100_parent     ,     100, ParentMap}
+map_insert_seq_bench!{insert_seq_100_std        ,     100, StdMap}
+
+map_find_rand_bench!{find_rand_100000_parent, 100_000, ParentMap}
+map_find_rand_bench!{find_rand_100000_std   , 100_000, StdMap}
+map_find_rand_bench!{find_rand_10000_parent ,  10_000, ParentMap}
+map_find_rand_bench!{find_rand_10000_std    ,  10_000, StdMap}
+map_find_rand_bench!{find_rand_100_parent   ,     100, ParentMap}
+map_find_rand_bench!{find_rand_100_std      ,     100, StdMap}
+
+map_find_seq_bench!{find_seq_100000_parent  , 100_000, ParentMap}
+map_find_seq_bench!{find_seq_100000_std     , 100_000, StdMap}
+map_find_seq_bench!{find_seq_10000_parent   ,  10_000, ParentMap}
+map_find_seq_bench!{find_seq_10000_std      ,  10_000, StdMap}
+map_find_seq_bench!{find_seq_100_parent     ,     100, ParentMap}
+map_find_seq_bench!{find_seq_100_std        ,     100, StdMap}
 
 map_iter_bench!{iter_100000_parent, 100_000, ParentMap}
 map_iter_bench!{iter_100000_std   , 100_000, StdMap}
